03c631d59b546139e3d80e9ecfb20558a6c77b93
Evan Seitz <evan.e.seitz@gmail.com>
2025-02-10 15:04:19 -0500

    update

--- seam/__init__.py
+++ seam/__init__.py
@@ -7,14 +7,15 @@ if package_root not in sys.path:
     sys.path.append(package_root)
 
 # Import key classes and functions
-from .meta_explainer import MetaExplainer
 from .compiler import Compiler
 from .attributer import Attributer
 from .clusterer import Clusterer
+from .meta_explainer import MetaExplainer
+from .identifier import Identifier
 from .utils import suppress_warnings, get_device
 
 # Version info
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # Expose key classes
 __all__ = [
